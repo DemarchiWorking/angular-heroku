b2b5176207968e60ce0e7c1c769576b08d7b48db
TonnyY <demarchivagas@gmail.com>
2021-01-08 11:47:33 -0300

    Host

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 	
-	const PORT = process.env.PORT || 8080;
+	const PORT = process.env.PORT;
     app.use(express.static(__dirname + '/src'));
 	app.get('/*',(req, res) => {
 		res.sendFile(__dirname +'/src/index.html');
